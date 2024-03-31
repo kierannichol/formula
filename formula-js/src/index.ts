6a1a349bec332443617ed61c4aaeb99465cb6c08
Kieran Nichol <knichol@popcap.com>
2024-03-30 17:46:24 -0700

    Added named and quoted resolved value to JS lib.

--- formula-js/src/index.ts
+++ formula-js/src/index.ts
@@ -1,6 +1,8 @@
 export { ResolvedValue } from "./ResolvedValue";
 export { ResolvedValueWithId } from "./ResolvedValueWithId";
 export { Resolvable } from "./Resolvable";
+export { NamedResolvedValue } from "./NamedResolvedValue";
+export { QuotedTextResolvedValue } from "./QuotedTextResolvedValue";
 export { DataContext, BaseDataContext, DataContextState, MutableDataContext, ImmutableDataContext } from "./DataContext";
 export { Formula } from "./Formula";
 export { ShuntingYard, ShuntingYardParser, Associativity } from "./ShuntingYard";
